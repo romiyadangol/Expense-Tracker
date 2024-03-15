0f29ab5903b5ea193d7ce67b383e6fc8fed76c00
romiyadangol <dangolromiya@gmail.com>
2024-03-15 19:59:51 +0545

    Update delete code

--- delete.php
+++ delete.php
@@ -1,3 +1,14 @@
 <?php
-
+     require "connect.php";
+        $expense_id = $_POST['id'];
+        $sql = "DELETE FROM exp WHERE id = $expense_id";
+        $result = mysqli_query($conn, $sql);
+        if($result){
+            echo "Expense deleted successfully";
+        }
+        else{
+            echo "Failed to delete expense";
+        }
+        header("Location: index.php");
+        exit();
 ?>
