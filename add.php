d33a1113e9b9fc544346c8cb1f59beceecc3835e
romiyadangol <dangolromiya@gmail.com>
2024-03-15 19:53:17 +0545

    Insert to form

---
+++ add.php
@@ -0,0 +1,15 @@
+<?php
+    require "./connect.php";
+    $name = $_POST['ename'];
+    $amount = $_POST['amt'];
+    $sql = "INSERT INTO exp (title, amount) VALUES ('$name','$amount')";
+    // $sql = "INSERT INTO exp (title, amount) VALUES ('test','100.00')";
+    $result = mysqli_query($conn, $sql);
+    if($result){
+        echo "New record created successfully";
+    }
+    else{
+        echo"Failed to Add Expense";
+    }
+ header("Location: index.php");
+ ?>
