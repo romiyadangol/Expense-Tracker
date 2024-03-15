da2840924e1022ddd514e1bc63e33e3c7990e6df
romiyadangol <dangolromiya@gmail.com>
2024-03-15 21:47:31 +0545

    Connection establish

--- connect.php
+++ connect.php
@@ -8,6 +8,6 @@
   if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
   }
-  else {
-    echo "Connected successfully";
-  }
+  // else {
+  //   echo "Connected successfully";
+  // }
